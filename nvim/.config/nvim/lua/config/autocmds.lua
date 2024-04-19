36cd977fba3bece22c6997a25d00c9d6ba406f11
David Michalik <david.michalik95@gmail.com>
2024-04-19 10:39:36 +0200

    added basic opts and autocmd to reload config files when they are changed

---
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,26 @@
+-- Function to unload a Lua module
+local function unload_module(module_name)
+  package.loaded[module_name] = nil
+end
+
+-- Function to reload Lua configuration modules
+local function reload_config()
+  -- Specify the modules you want to reload, e.g., 'config.settings'
+  local modules_to_reload = {
+    'config.options',
+    'config.keymaps'
+  }
+  -- Unloading and reloading each module
+  for _, module in ipairs(modules_to_reload) do
+    unload_module(module)
+    require(module)
+  end
+  print("Configuration reloaded!")
+end
+
+-- Set up the autocmd
+vim.api.nvim_create_autocmd("BufWritePost", {
+    -- Adjust the pattern to match the path to your lua/config directory
+    pattern = "*/lua/config/*",
+    callback = reload_config,
+})
---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,17 @@
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.scrolloff = true
+
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
+
+vim.opt.title = true
+vim.opt.scrolloff = 10
+vim.opt.cursorline = true
+
+vim.opt.clipboard=unnamedplus
