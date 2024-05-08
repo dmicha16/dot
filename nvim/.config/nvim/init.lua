bfdbe3ea6821eb9380a5d806fe94162f956fcbb8
David Michalik <david.michalik95@gmail.com>
2024-05-08 10:15:53 +0200

    fixed lazy vim config and added theme and neotree

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,8 +2,7 @@ require("config.options")
 require("config.keymaps")
 require("config.autocmds")
 
-
--- bootstrap lazy.nvim
+-- install lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -17,6 +16,38 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(plugins, opts)
 
-require("config.netrw")
+-- plugins
+require("lazy").setup({
+	{
+         "folke/which-key.nvim",
+	  event = "VeryLazy",
+	  init = function()
+	    vim.o.timeout = true
+	    vim.o.timeoutlen = 300
+	  end,
+	  opts = {
+	    -- your configuration comes here
+	    -- or leave it empty to use the default settings
+	    -- refer to the configuration section below
+	  }
+        },
+	{
+	 "nvim-neo-tree/neo-tree.nvim",
+	 branch = "v3.x",
+	 dependencies = {
+	      "nvim-lua/plenary.nvim",
+	      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+	      "MunifTanjim/nui.nvim",
+	      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	 }
+	},
+	"rebelot/kanagawa.nvim"
+})
+
+
+require("plugins.colorscheme")
+
+
+-- enable the colorscheme
+vim.cmd("colorscheme kanagawa")
--- nvim/.config/nvim/lua/config/netrw.lua
+++ nvim/.config/nvim/lua/config/netrw.lua
@@ -1,6 +1,6 @@
 -- netrw settings
 vim.g.netrw_liststyle = 3
-vim.g.netrw_browse_split = 4
+vim.g.netrw_browse_split = 2
 vim.g.netrw_altv = 1
 vim.g.netrw_winsize = 15
 vim.g.netrw_list_hide = '\\.git$,\\*~$'
